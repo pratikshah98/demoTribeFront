7830b56340b9132e3387c41b728ebf1a767eca52
pratikshah98 <pratikshah012325@gmail.com>
2021-12-26 22:52:37 +0530

    changes in frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+ import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontdemo';
+export class AppComponent implements OnInit{
+  final_date:Date;
+  flag:Boolean= false;
+  startDate = new Date(Date.now());
+  final_arr=[];
+  final_data=[];
+  final_final=[];
+  final_only_push:any = [];
+  check_date:Date;
+  i:number=0;
+  constructor(private _http:HttpClient,private datepipe:DatePipe){}
+   ngOnInit() {
+    this.onChangeDate(this.startDate);    
+}
+onChangeDate(data:any){
+   
+  let check_data = this.datepipe.transform(data, 'YYYY-MM-dd');
+
+    this._http.get<any>('https://www.gov.uk/bank-holidays.json').subscribe(data => {
+      this.final_arr = Object.values(data);
+      for (this.i = 0; this.i < this.final_arr.length; this.i++) {
+        this.final_data = this.final_arr[this.i]['events'];
+        this.final_final = this.final_data.filter(item => {
+          return item['date'] == check_data
+        })     
+      }
+      if(this.final_final.length > 0){
+        this.final_only_push.push(...this.final_final)
+        this.flag = true;
+     }else{
+        this.flag=false;
+     }
+    });
+   }
+   onUpdateDate(){
+    this.final_only_push=[];
+     this.onChangeDate(this.final_date);
+   }
+
+   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { DatePipe } from '@angular/common'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatInputModule,
+    MatTableModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    ReactiveFormsModule
+
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
